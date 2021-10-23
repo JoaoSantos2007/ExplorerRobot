204dc5a5e914c9fc6ac0a936cce6e0889007eac7
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-10-23 17:53:05 -0300

    Update ExplorerRobot.ino

--- ExplorerRobot.ino
+++ ExplorerRobot.ino
@@ -1,3 +1,18 @@
+#include <ExplorerRobot.h>
+#include <Ultrasonic.h>
+
+//Motor 1
+int motor1V = ;
+int motor1A = ;
+int motor1B = ;
+
+//Motor 2
+int motor2V = ;
+int motor2A = ;
+int motor2B = ;
+
+ExplorerRobot robot()
+
 void setup() {
   // put your setup code here, to run once:
 
